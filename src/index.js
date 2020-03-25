afb1613e35bccbe99422ce694a5cb13436387f2a
Donawalt <donael.walter@hotmail.com>
2020-03-25 12:27:09 +0100

    update delay

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const intersectionObserverSection = new IntersectionObserver(
       if (entry.isIntersecting) {
         // element is in viewport, do the stuff
         console.log(entry);
-        gsap.fromTo(entry.target, 1, {opacity:0},{opacity: 1, delay: 0.8});
+        gsap.fromTo(entry.target, 1, {opacity:0},{opacity: 1, delay: 0.7});
         // it's good to remove observer,
         // if you don't need it any more
         observer.unobserve(entry.target);
